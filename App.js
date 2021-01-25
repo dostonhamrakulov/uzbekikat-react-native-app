41114ca25fe72dc75d3ca4f64206513ee7eac828
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2021-01-25 23:46:40 +0100

    Adding two screen and navigation between them

--- App.js
+++ App.js
@@ -1,61 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, YellowBox } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import HomeActivity from './src/HomeActivity';
+import ProfileActivity from './src/ProfileActivity';
+const RootStack = createStackNavigator(
+    {
+        Home: { screen: HomeActivity },
+        Profile: { screen: ProfileActivity },
+    },/**/
+    {
+        initialRouteName: 'Home',
+    }
+);
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <View style={styles.welcomeDiv}>
-        <Text style={styles.textHeader}>Uzbek ikat</Text>
-        <Text style={styles.textBody}>
-          We are coming <Text style={styles.textBold}>soon</Text>.
-        </Text>
-      </View>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  welcomeDiv: {
-    marginTop: 250,
-  },
-  textHeader: {
-    fontSize: 30,
-    textAlign: 'center',
-  },
-  textBody: {
-    marginTop: 23,
-    fontSize: 23,
-    textAlign: 'center',
-  },
-
-  textBold: {
-    fontSize: 23,
-    textAlign: 'center',
-    fontWeight: 'bold',
-  }
-});
+const App = createAppContainer(RootStack);
 
 export default App;
